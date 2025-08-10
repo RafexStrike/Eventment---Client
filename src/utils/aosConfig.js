8693c6d1a35f6f7b9ef39dbf0ed5adaa7b533c95
rafexstrike <adnanrahmanrafi515@gmail.com>
2025-08-10 19:39:56 +0600

    asoConfig.js settings changed to resolve the aos issue.

--- src/utils/aosConfig.js
+++ src/utils/aosConfig.js
@@ -3,21 +3,19 @@ import 'aos/dist/aos.css';
 
 export const initAOS = () => {
   AOS.init({
-    startEvent: 'load',
-    once: false,
-    duration: 1000,
-    offset: 50,
+    duration: 800,
+    once: false, // Allow animations to trigger multiple times
+    offset: 100, // Trigger animations 100px before element enters viewport
     delay: 0,
-    mirror: true,
-    anchorPlacement: 'top-bottom',
-    disable: false,
-    useClassNames: true,
-    disableMutationObserver: false,
-    debounceDelay: 50,
-    throttleDelay: 99,
+    easing: 'ease-in-out',
+    mirror: false,
+    anchorPlacement: 'top-bottom'
   });
 };
 
 export const refreshAOS = () => {
-  AOS.refresh();
-}; 
+  // Add a small delay to ensure DOM is updated
+  setTimeout(() => {
+    AOS.refresh();
+  }, 100);
+};
